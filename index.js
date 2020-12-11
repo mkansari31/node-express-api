c683645c51bb157fbe2fe5bc91945fe3bce5c4e2
Kashif-Ansari <mkansari515@gmail.com>
2020-12-11 20:25:40 +0530

    name added

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-// app.use(express.json());
-
+// azhar
 
 const courses = [
     {'id': 1 , 'name': 'course1'},
